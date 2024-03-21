a34f2a0153254ffe07222ab5b483db32a14bafae
15693 <zachary.stevens@vtsd.com>
2024-03-21 07:57:02 -0400

    step 26

--- 2-1-4/tkpractice.py
+++ 2-1-4/tkpractice.py
@@ -1,14 +1,18 @@
 #   a214_simple_window1.py
 #   A program creates a window on your screen using Tkinter.
 import tkinter as tk
-
+#Test of button
+def test_my_button():
+    frame_auth.tkraise()
+    print("yes")
 # main window
 root = tk.Tk()
 root.wm_geometry("200x200")
-
 # create empty frame
 frame_login = tk.Frame(root)
-frame_login.grid()
+frame_login.grid(row = 0, column = 0, sticky = "news")
+frame_auth = tk.Frame(root)
+frame_auth.grid(row = 0, column = 0, sticky = "news")
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack()
@@ -22,8 +26,10 @@ lbl_password.pack()
 ent_password = tk.Entry(frame_login, bd=3)
 ent_password.pack(pady=5)
 
-btn_login = tk.Label(frame_login, text='Login')
+btn_login = tk.Button(frame_login, text='Login', command=test_my_button)
 btn_login.pack(pady=5)
 
+tk.Label(frame_login,text="Password:",font="Courier")
 
+frame_login.tkraise()
 root.mainloop()
